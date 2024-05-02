79e2649a7248479b4af142ec46c5fe7bcc8b80de
macluiggy <luiggy.macias@imagineapps.co>
2024-05-01 19:33:38 -0500

    Update .dockerignore and .gitignore files

--- backend/src/common/config.ts
+++ backend/src/common/config.ts
@@ -5,13 +5,18 @@ dotenv.config();
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const isProduction = NODE_ENV === 'production';
-const DB_HOST = isProduction ? process.env.DB_HOST_PROD : process.env.DB_HOST;
-const DB_USER = isProduction ? process.env.DB_USER_PROD : process.env.DB_USER;
-const DB_PASSWORD = isProduction
-  ? process.env.DB_PASSWORD_PROD
-  : process.env.DB_PASSWORD;
-const DB_NAME = isProduction ? process.env.DB_NAME_PROD : process.env.DB_NAME;
-const DB_PORT = isProduction ? process.env.DB_PORT_PROD : process.env.DB_PORT;
+// const DB_HOST = isProduction ? process.env.DB_HOST_PROD : process.env.DB_HOST;
+// const DB_USER = isProduction ? process.env.DB_USER_PROD : process.env.DB_USER;
+// const DB_PASSWORD = isProduction
+//   ? process.env.DB_PASSWORD_PROD
+//   : process.env.DB_PASSWORD;
+// const DB_NAME = isProduction ? process.env.DB_NAME_PROD : process.env.DB_NAME;
+// const DB_PORT = isProduction ? process.env.DB_PORT_PROD : process.env.DB_PORT;
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const DB_PORT = process.env.DB_PORT;
 
 const config = {
   apiVersion: process.env.API_VERSION || 'v1',
@@ -24,6 +29,7 @@ const config = {
     username: DB_USER || 'postgres',
   },
   nodeEnviroment: NODE_ENV,
+  isProduction,
 };
 
 export default config;
