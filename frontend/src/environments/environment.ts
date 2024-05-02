ccef8879f3199741e9d5fa8e10227ee4273508d8
macluiggy <luiggy.macias@imagineapps.co>
2024-05-02 15:43:30 -0500

    Refactor Dockerfile to update base image to node:22-alpine3.1

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000/api/v1',
+  // apiUrl: 'http://localhost:4000/api/v1',
+  apiUrl: 'https://social-media-sosote.up.railway.app/api/v1',
 };
