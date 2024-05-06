0611b7ca1e7c81dabea0893b4a77589d99905e9f
macluiggy <luiggy.macias@imagineapps.co>
2024-05-06 18:50:12 -0500

    feat: Add vitest e2e test configuration

--- backend/src/posts/posts.controller.spec.ts
+++ backend/src/posts/posts.controller.spec.ts
@@ -1,22 +1,51 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { PostsController } from './posts.controller';
-// import { PostsService } from './posts.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
+import { Post } from './entities/post.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
-// describe('PostsController', () => {
-//   let controller: PostsController;
-//   let service: any;
+// Mock implementation of the Post repository
+const mockPostRepository = {
+  find: jest.fn(),
+  findOneBy: jest.fn(),
+  save: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn(),
+};
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       controllers: [PostsController],
-//       providers: [PostsService],
-//     }).compile();
+// Mock implementation of the Request object
+const mockRequest = {
+  preferredLanguage: 'en', // Set the desired language for testing
+};
 
-//     controller = module.get<PostsController>(PostsController);
-//     service = module.resolve<PostsService>(PostsService);
-//   });
+describe('PostsController', () => {
+  let controller: PostsController;
+  let service: PostsService;
 
-//   it('should be defined', () => {
-//     expect(controller).toBeDefined();
-//   });
-// });
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PostsController],
+      providers: [
+        PostsService,
+        {
+          provide: getRepositoryToken(Post),
+          useValue: mockPostRepository,
+        },
+        {
+          provide: 'REQUEST',
+          useValue: mockRequest,
+        },
+      ],
+    }).compile();
+
+    controller = await module.resolve<PostsController>(PostsController);
+    service = await module.resolve<PostsService>(PostsService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  // Add more tests for your controller and service methods
+});
