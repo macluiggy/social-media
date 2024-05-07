e21199130d6e174922f59dff3a7540535379c5cd
macluiggy <luiggy.macias@imagineapps.co>
2024-05-06 20:09:35 -0500

    feat: Add MOCK_REQUEST constant for testing purposes

--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -1,5 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from './users.entity';
+import { DataSource } from 'typeorm';
+import { vi } from 'vitest';
+import { MOCK_REQUEST } from '../common/tests/constants';
+
+const mockUsersRepository = {
+  find: vi.fn(),
+  findOneBy: vi.fn(),
+  save: vi.fn(),
+  update: vi.fn(),
+  delete: vi.fn(),
+};
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -7,9 +21,24 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: 'REQUEST',
+          useValue: MOCK_REQUEST,
+        },
+        {
+          provide: 'UsersRepository',
+          useValue: mockUsersRepository,
+        },
+        {
+          provide: DataSource,
+          useValue: {},
+        },
+      ],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    controller = await module.resolve<UsersController>(UsersController);
   });
 
   it('should be defined', () => {
--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,15 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { vi } from 'vitest';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from './users.entity';
+import { DataSource } from 'typeorm';
+import { MOCK_REQUEST } from '../common/tests/constants';
 
+const mockUsersRepository = {
+  find: vi.fn(),
+  findOneBy: vi.fn(),
+  save: vi.fn(),
+  update: vi.fn(),
+  delete: vi.fn(),
+};
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: getRepositoryToken(Users), useValue: mockUsersRepository },
+        { provide: DataSource, useValue: {} },
+        { provide: 'REQUEST', useValue: MOCK_REQUEST },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    service = await module.resolve<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
