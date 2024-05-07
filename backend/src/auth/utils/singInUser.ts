a77db148974b6ae162897820b269bd7c41201307
macluiggy <luiggy.macias@imagineapps.co>
2024-05-07 11:22:22 -0500

    chore: Refactor signInUser function to use Singleton pattern

--- backend/src/auth/utils/singInUser.ts
+++ backend/src/auth/utils/singInUser.ts
@@ -9,16 +9,30 @@ type SingInUserResponse = {
   accessToken: string;
   user: any;
 };
+
+class Singleton {
+  private static instance: SingInUserResponse | null = null;
+
+  private constructor(private app: INestApplication) {}
+
+  public static async getInstance(
+    app: INestApplication,
+  ): Promise<SingInUserResponse> {
+    if (!this.instance) {
+      const endpoint = getApiEndpoint('auth/signin');
+      const response = await request(app.getHttpServer())
+        .post(endpoint)
+        .send({ email: EMAIL, password: PASSWORD });
+
+      this.instance = response.body?.data;
+    }
+
+    return this.instance;
+  }
+}
+
 export async function signInUser(
   app: INestApplication,
-  email: string = EMAIL,
-  password: string = PASSWORD,
 ): Promise<SingInUserResponse> {
-  const endpoint = getApiEndpoint('auth/signin');
-  const response = await request(app.getHttpServer())
-    .post(endpoint)
-    .send({ email, password });
-
-  const data = response.body?.data;
-  return data;
+  return Singleton.getInstance(app);
 }
