622d9216ccc97cda008aa1ad283bd885d5e66789
macluiggy <luiggy.macias@imagineapps.co>
2024-05-07 19:27:35 -0500

    refactor: Update e2e test for root endpoint to include response body

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,24 +1,22 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { AppModule } from './../src/app.module';
+import setupTestingModule from './setUpTestingModule';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    const testingModule = await setupTestingModule();
+    app = testingModule.app;
 
-    app = moduleFixture.createNestApplication();
     await app.init();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect({
+      statusCode: 200,
+      data: 'Hello World!',
+      message: 'OK',
+    });
   });
 });
