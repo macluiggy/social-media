2e4d6bdb42a359b97c4b64682c96493e0844154a
macluiggy <luiggy.macias@imagineapps.co>
2024-05-09 18:32:42 -0500

    refactor: Update home component to conditionally show posts when user is logged in

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,20 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user/user.service';
 import { environment } from '../../environments/environment';
 // impot button module primeng
-import {ButtonModule} from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
+import { StorageService } from '../services/storage/storage.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ ButtonModule],
+  imports: [ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
-  content?: string
+  content?: string;
+  isLoggedIn = this.storageService.isLoggedIn();
 
   test = environment.test;
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private storageService: StorageService
+  ) {}
 
   ngOnInit(): void {
     this.userService.getPublicContent().subscribe({
