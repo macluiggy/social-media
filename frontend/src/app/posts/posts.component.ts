e8f45b8a2af0e87e47b3ee459ab9020d76e66470
macluiggy <luiggy.macias@imagineapps.co>
2024-05-10 12:57:18 -0500

    refactor: Improve loading condition in posts component HTML

--- frontend/src/app/posts/posts.component.ts
+++ frontend/src/app/posts/posts.component.ts
@@ -6,6 +6,8 @@ import { CommonModule, NgFor } from '@angular/common';
 import { VirtualScrollerModule } from 'primeng/virtualscroller';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { CustomProgressLoadingComponent } from '../custom-progress-loading/custom-progress-loading.component';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+
 @Component({
   selector: 'app-posts',
   standalone: true,
@@ -15,24 +17,18 @@ import { CustomProgressLoadingComponent } from '../custom-progress-loading/custo
     NgFor,
     VirtualScrollerModule,
     InfiniteScrollModule,
-    CustomProgressLoadingComponent
+    CustomProgressLoadingComponent,
+    ProgressSpinnerModule,
   ],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss',
 })
 export class PostsComponent implements OnInit, OnDestroy {
   randomPosts: RandomPosts[] = [
-    {
-      content: 'content',
-      title: 'title',
-      user: {
-        fullName: 'fullName',
-        username: 'username',
-      },
-    },
   ] as RandomPosts[];
   private page = 1;
   private limit = 5;
+  firstLoad = true;
   loading = false;
 
   constructor(private postsService: PostsService) {
@@ -46,18 +42,21 @@ export class PostsComponent implements OnInit, OnDestroy {
 
   private fetchPosts() {
     this.loading = true;
-    this.postsService.getRandomPosts({ limit: 5, page: this.page }).subscribe({
-      next: (response: any) => {
-        this.randomPosts = [...this.randomPosts, ...response.data.items];
-        this.page++;
-      },
-      error: (error) => {
-        console.error(error);
-      },
-      complete: () => {
-        this.loading = false;
-      },
-    });
+    this.postsService
+        .getRandomPosts({ limit: 5, page: this.page })
+        .subscribe({
+          next: (response: any) => {
+            this.randomPosts = [...this.randomPosts, ...response.data.items];
+            this.page++;
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => {
+            this.loading = false;
+            this.firstLoad = false;
+          },
+        });
   }
 
   onScroll() {
