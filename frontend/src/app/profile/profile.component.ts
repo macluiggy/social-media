48fc89b2a1a45581106fcb9a87bd87594bbfdd49
macluiggy <luiggy.macias@imagineapps.co>
2024-05-13 17:07:47 -0500

    refactor: Update profile component to use PrimeNG card for user profile display

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../services/storage/storage.service';
 import { ActivatedRoute } from '@angular/router';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CardModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
   currentUser: any;
   userId: string;
 
@@ -18,9 +19,11 @@ export class ProfileComponent implements OnInit {
     private activatedRoute: ActivatedRoute
   ) {
     this.userId = this.activatedRoute.snapshot.params['userId'];
-  }
-
-  ngOnInit(): void {
     this.currentUser = this.storageService.getUser();
+
+    if (!this.currentUser.profilePhoto) {
+      this.currentUser.profilePhoto =
+        'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png';
+    }
   }
 }
