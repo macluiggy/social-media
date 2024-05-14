61f9caf6575f1ba52d12d8d1691780b42d9ed7f2
macluiggy <luiggy.macias@imagineapps.co>
2024-05-14 16:01:41 -0500

    refactor: Remove IA API module and related files

--- backend/src/ia-api/dto/update-ia-api.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateIaApiDto } from './create-ia-api.dto';
-
-export class UpdateIaApiDto extends PartialType(CreateIaApiDto) {}
--- backend/src/ia-api/ia-api.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IaApiService } from './ia-api.service';
-import { IaApiController } from './ia-api.controller';
-
-@Module({
-  controllers: [IaApiController],
-  providers: [IaApiService],
-})
-export class IaApiModule {}
