fc155bf9c3093aee70acdb6643b8506d13fa9c75
macluiggy <luiggy.macias@imagineapps.co>
2024-05-15 10:17:00 -0500

    refactor: Update HuggingFaceStrategy to use chatCompletionStream for text generation

--- backend/src/ai-api/strategy/huggingface.strategy.ts
+++ backend/src/ai-api/strategy/huggingface.strategy.ts
@@ -13,6 +13,24 @@ export default class HuggingFaceStrategy implements AiStrategy {
   }
   // later add the constructor to add the configuration from google to add the api key
   async createCompletion(prompt: string): Promise<string> {
+    // let out = '';
+    // for await (const chunk of this.hfInference.chatCompletionStream({
+    //   model: 'mistralai/Mistral-7B-Instruct-v0.2',
+    //   messages: [
+    //     {
+    //       role: 'user',
+    //       content: 'Complete the equation 1+1= ,just the answer',
+    //     },
+    //   ],
+    //   max_tokens: 500,
+    //   temperature: 0.1,
+    //   seed: 0,
+    // })) {
+    //   if (chunk.choices && chunk.choices.length > 0) {
+    //     out += chunk.choices[0].delta.content;
+    //     console.log(out);
+    //   }
+    // }
     const response = await this.hfInference.textGeneration({
       inputs: prompt,
     });
