af12e2069c64711b555230bfafdbe61bde427cd4
macluiggy <luiggy.macias@imagineapps.co>
2024-05-15 10:40:08 -0500

    refactor: Update AI API to use chatCompletion method for text generation

--- backend/src/ai-api/ai-api.controller.ts
+++ backend/src/ai-api/ai-api.controller.ts
@@ -1,40 +1,4 @@
-import {
-  Controller,
-  // Get,
-  // Post,
-  // Body,
-  // Patch,
-  // Param,
-  // Delete,
-} from '@nestjs/common';
-// import { AiApiService } from './ai-api.service';
-// import { CreateAiApiDto } from './dto/create-ai-api.dto';
-// import { UpdateAiApiDto } from './dto/update-ai-api.dto';
+import { Controller } from '@nestjs/common';
 
 @Controller('ai-api')
-export class AiApiController {
-  // aiApiService: AiApiService;
-  // constructor(aiApiService: AiApiService) {
-  //   this.aiApiService = aiApiService;
-  // }
-  // @Post()
-  // create(@Body() createAiApiDto: CreateAiApiDto) {
-  //   return this.aiApiService.create(createAiApiDto);
-  // }
-  // @Get()
-  // findAll() {
-  //   return this.aiApiService.findAll();
-  // }
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.aiApiService.findOne(+id);
-  // }
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateAiApiDto: UpdateAiApiDto) {
-  //   return this.aiApiService.update(+id, updateAiApiDto);
-  // }
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.aiApiService.remove(+id);
-  // }
-}
+export class AiApiController {}
--- backend/src/ai-api/ai-api.service.ts
+++ backend/src/ai-api/ai-api.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import AiStrategy from './strategy/ai-strategy';
 import Context from './strategy/context';
-// import GeminiStrategy from './strategy/gemini.strategy';
 import HuggingFaceStrategy from './strategy/huggingface.strategy';
-// import { CreateAiApiDto } from './dto/create-ai-api.dto';
-// import { UpdateAiApiDto } from './dto/update-ai-api.dto';
 
 @Injectable()
 export class AiApiService implements AiStrategy {
@@ -24,19 +21,4 @@ export class AiApiService implements AiStrategy {
   }): Promise<{ outputResponse: string }> {
     return this.context.chatCompletion({ prompt });
   }
-  // create(createAiApiDto: CreateAiApiDto) {
-  //   return 'This action adds a new aiApi';
-  // }
-  // findAll() {
-  //   return `This action returns all aiApi`;
-  // }
-  // findOne(id: number) {
-  //   return `This action returns a #${id} aiApi`;
-  // }
-  // update(id: number, updateAiApiDto: UpdateAiApiDto) {
-  //   return `This action updates a #${id} aiApi`;
-  // }
-  // remove(id: number) {
-  //   return `This action removes a #${id} aiApi`;
-  // }
 }
