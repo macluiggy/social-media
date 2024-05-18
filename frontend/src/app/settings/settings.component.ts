e78e74f3232a212427bb92e3fceada4b07084a48
macluiggy <luiggy.macias@imagineapps.co>
2024-05-17 20:26:51 -0500

    feat: Add profile component to settings
    This commit adds the profile component to the settings section of the frontend application. The profile component consists of an HTML template, a TypeScript file, and a spec file for testing. The HTML template contains a simple paragraph element with the text "profile works!". The component is standalone and does not have any imports. The component is created using Angular's @Component decorator and has a selector of 'app-profile'. The template and styles are referenced using templateUrl and styleUrl properties respectively. This addition is necessary to implement the desired profile functionality in the settings section.

--- frontend/src/app/settings/settings.component.ts
+++ frontend/src/app/settings/settings.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { TabViewModule } from 'primeng/tabview';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { TabView } from 'primeng/tabview';
+import { ProfileComponent } from '../profile/profile.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  providers: [TabView, ProfileComponent],
+  imports: [TabViewModule, TabMenuModule],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrl: './settings.component.scss',
 })
-export class SettingsComponent {
-
-}
+export class SettingsComponent {}
