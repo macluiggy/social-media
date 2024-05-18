4d939c0dbe7f1403bc29a8547beaab313afb598b
macluiggy <luiggy.macias@imagineapps.co>
2024-05-17 21:24:09 -0500

    feat: Add profile settings form to settings component
    This commit adds a form for profile settings to the settings component. The form includes fields for first name, last name, username, email, and profile picture. It also includes a submit button to save the changes. This addition is necessary to allow users to update their profile information in the settings section.

--- frontend/src/app/settings/profile-settings/profile-settings.component.ts
+++ frontend/src/app/settings/profile-settings/profile-settings.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { User } from '../../common/types';
+// import for <p-inputText
+import { InputTextModule } from 'primeng/inputtext';
+import { FileUploadModule } from 'primeng/fileupload';
 
 @Component({
   selector: 'app-profile-settings',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    CardModule,
+    ButtonModule,
+    InputTextModule,
+    FileUploadModule,
+  ],
   templateUrl: './profile-settings.component.html',
-  styleUrl: './profile-settings.component.scss'
+  styleUrl: './profile-settings.component.scss',
 })
 export class ProfileSettingsComponent {
+  @ViewChild('profilePicture') profilePicture: any;
 
+  profileForm: FormGroup;
+  constructor() {
+    this.profileForm = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+      id: new FormControl(''),
+      username: new FormControl(''),
+      // profile picture is a file
+      profilePicture: new FormControl('', {}),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.profileForm.value);
+  }
+
+  onSelect(event: any) {
+    console.log(this.profileForm.value);
+
+    for (const file of event.files) {
+      this.profileForm.patchValue({
+        profilePicture: file,
+      });
+      this.profileForm.get('profilePicture')?.updateValueAndValidity();
+    }
+
+    console.log(this.profileForm.value);
+  }
 }
