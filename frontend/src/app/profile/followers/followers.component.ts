35dfd83d3955b5dd0566048e20d6c5e11ccbc4b9
macluiggy <luiggy.macias@imagineapps.co>
2024-05-21 12:12:55 -0500

    feat: Update followers and following components in profile page
    This commit updates the followers and following components in the profile page. The HTML and TypeScript files for both components have been modified to improve the layout and functionality. The followers component now displays user cards in a grid layout, and the following component has been optimized to fetch data only when the user ID changes. These changes enhance the user experience by providing a more visually appealing and efficient way to view and interact with followers and following lists on the profile page.

--- frontend/src/app/profile/followers/followers.component.ts
+++ frontend/src/app/profile/followers/followers.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { UserWithFollows } from '../../common/types/user.type';
+import { FollowsService } from '../../services/follows/follows.service';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-followers',
   standalone: true,
-  imports: [],
+  imports: [CardModule],
   templateUrl: './followers.component.html',
-  styleUrl: './followers.component.scss'
+  styleUrl: './followers.component.scss',
 })
-export class FollowersComponent {
+export class FollowersComponent implements OnChanges {
+  @Input() userId!: number;
+  userFollowers: UserWithFollows[] = [];
+  constructor(private followService: FollowsService) {
+    this.getUserFollowers();
+  }
 
+  ngOnChanges() {
+    this.getUserFollowers();
+  }
+
+  getUserFollowers() {
+    if (!this.userId) return;
+    this.followService.getUserFollowers(this.userId).subscribe((res: any) => {
+      this.userFollowers = res.data;
+      console.log(this.userFollowers);
+      
+    });
+  }
 }
