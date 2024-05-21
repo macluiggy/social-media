bc48d98549daa0dc62c1989efdf4d5eb19a2f838
macluiggy <luiggy.macias@imagineapps.co>
2024-05-21 09:45:55 -0500

    feat: Add tabbed view for followers and following in profile page
    This commit adds a tabbed view to the profile page for displaying followers and following. The follows component has been replaced with a tabView component from the primeng library. The followers and following components are now displayed in separate tab panels, providing a more organized and intuitive user experience.

--- frontend/src/app/profile/follows/follows.component.ts
+++ frontend/src/app/profile/follows/follows.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { TabView, TabViewModule } from 'primeng/tabview';
+import { FollowingComponent } from '../following/following.component';
+import { FollowersComponent } from '../followers/followers.component';
 
 @Component({
   selector: 'app-follows',
   standalone: true,
-  imports: [],
+  providers: [TabView],
+  imports: [DialogModule, TabViewModule, FollowersComponent, FollowingComponent],
   templateUrl: './follows.component.html',
-  styleUrl: './follows.component.scss'
+  styleUrl: './follows.component.scss',
 })
-export class FollowsComponent {
-
-}
+export class FollowsComponent {}
