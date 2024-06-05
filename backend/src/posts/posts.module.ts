079fc71cdb8119fcd69a030ffd7130963f34530b
macluiggy <luiggy.macias@imagineapps.co>
2024-06-05 09:46:28 -0500

    feat: Add like module and controller

--- backend/src/posts/posts.module.ts
+++ backend/src/posts/posts.module.ts
@@ -3,9 +3,10 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
+import { LikesModule } from './likes/likes.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post]), LikesModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
