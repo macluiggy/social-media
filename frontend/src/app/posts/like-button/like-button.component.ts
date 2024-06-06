be5e9ff4d04095f699699439e6b240b8a502e9c8
macluiggy <luiggy.macias@imagineapps.co>
2024-06-06 14:54:15 -0500

    refactor: Update like button component and related files

--- frontend/src/app/posts/like-button/like-button.component.ts
+++ frontend/src/app/posts/like-button/like-button.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-like-button',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './like-button.component.html',
   styleUrl: './like-button.component.scss'
 })
 export class LikeButtonComponent {
-
+  liked = true;
+  likePost() {
+    // Like the post
+  }
 }
