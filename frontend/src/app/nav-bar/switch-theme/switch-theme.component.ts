c0b0475f940e650eb1adcacc2ccb47f5cc6c432c
macluiggy <luiggy.macias@imagineapps.co>
2024-06-13 10:31:04 -0500

    Refactor NavBarComponent to improve menu item rendering

---
+++ frontend/src/app/nav-bar/switch-theme/switch-theme.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-theme',
+  standalone: true,
+  imports: [],
+  templateUrl: './switch-theme.component.html',
+  styleUrl: './switch-theme.component.scss'
+})
+export class SwitchThemeComponent {
+
+}
