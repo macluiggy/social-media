72082d3a8947439376c8fd60a79cf1f1d30e77ce
macluiggy <luiggy.macias@imagineapps.co>
2024-06-14 09:25:48 -0500

    Refactor CreateCommentDto to include validation decorators and Swagger documentation

--- backend/src/posts/comments/dto/create-comment.dto.ts
+++ backend/src/posts/comments/dto/create-comment.dto.ts
@@ -1,1 +1,21 @@
-export class CreateCommentDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateCommentDto {
+  @ApiProperty({ example: 'This is a comment' })
+  @IsNotEmpty({
+    message: 'Please enter a comment',
+  })
+  @IsString()
+  content: string;
+
+  @ApiProperty({ example: 1 })
+  @IsNotEmpty()
+  @IsNumber()
+  postId: number;
+
+  @ApiProperty({ example: 1 })
+  @IsOptional()
+  @IsNumber()
+  parentCommentId?: number;
+}
