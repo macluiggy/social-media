2f1474db438c9a895c29460dc14c2e015d76a7cf
macluiggy <luiggy.macias@imagineapps.co>
2024-06-19 10:08:05 -0500

    chore: Update environment configuration for development, production, and staging
    This commit updates the environment configuration files for development, production, and staging. It modifies the `apiUrl` property in each file to use a dynamic `baseUrl` variable instead of hardcoding the URLs. This change allows for easier management and flexibility when switching between different environments. Additionally, it adds the `baseUrl` variable to each environment file to store the common base URL. This update improves the maintainability and scalability of the application's environment configuration.

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,7 +1,10 @@
+const baseUrl = 'http://localhost:3000';
+// apiUrl: 'http://localhost:3000/api/v1',
+// apiUrl: 'https://backend-green-three-34.vercel.app/api/v1'
+// apiUrl: 'https://srng5l6h-3000.use2.devtunnels.ms/api/v1',
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:3000/api/v1',
-  apiUrl: 'https://srng5l6h-3000.use2.devtunnels.ms/api/v1',
+  apiUrl: `${baseUrl}/api/v1`,
+  baseUrl,
   test: 'DEVELOPMENT ENVIRONMENT',
-  // apiUrl: 'https://backend-green-three-34.vercel.app/api/v1'
 };
--- frontend/src/environments/environment.staging.ts
+++ frontend/src/environments/environment.staging.ts
@@ -1,6 +1,10 @@
+const baseUrl = 'https://backend-macluiggy-macluiggys-projects.vercel.app';
+// apiUrl: 'https://social-media-staging.up.railway.app/api/v1',
+// apiUrl: 'https://backend-macluiggy-macluiggys-projects.vercel.app/api/v1',
+
 export const environment = {
-    production: false,
-    // apiUrl: 'https://social-media-staging.up.railway.app/api/v1',
-    apiUrl: 'https://backend-macluiggy-macluiggys-projects.vercel.app/api/v1',
-    test: 'STAGING ENVIRONMENT', // This is the only line that is different
+  production: false,
+  apiUrl: `${baseUrl}/api/v1`,
+  baseUrl,
+  test: 'STAGING ENVIRONMENT', // This is the only line that is different
 };
