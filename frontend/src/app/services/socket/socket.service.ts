22a48c00ef397bcc52981f3f827bc079b8e809bc
macluiggy <luiggy.macias@imagineapps.co>
2024-06-19 10:09:20 -0500

    feat: Add frontend messages component
    This commit adds the frontend messages component, including the HTML template, TypeScript file, and unit test. The messages component is responsible for displaying messages in the application. This feature enhances the user experience by providing a dedicated component for managing and displaying messages.

--- frontend/src/app/services/socket/socket.service.ts
+++ frontend/src/app/services/socket/socket.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SocketService {
+  private socket: Socket;
 
-  constructor() { }
+  constructor() {
+    this.socket = io(environment.baseUrl);
+  }
+
+  sendMessage(message: { sender: string; content: string }): void {
+    this.socket.emit('sendMessage', message);
+  }
+
+  onMessage(): Observable<{ sender: string; content: string }> {
+    return new Observable((observer) => {
+      this.socket.on('receiveMessage', (message) => {
+        observer.next(message);
+      });
+    });
+  }
 }
