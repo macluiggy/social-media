cea9e58df595fc5de94712b80d455016f4421396
macluiggy <luiggy.macias@imagineapps.co>
2024-06-20 10:46:20 -0500

    chore: Update base URLs for staging environments
    This commit updates the base URLs for the staging environments in the frontend/src/environments/environment.development.ts and frontend/src/environments/environment.staging.ts files. The previous URLs have been replaced with the correct URLs for the social media staging environment. This change ensures that the frontend application connects to the correct backend API endpoints in the staging environment.

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,7 +1,10 @@
 const baseUrl = 'http://localhost:3000';
-// apiUrl: 'http://localhost:3000/api/v1',
+// staging for testing
+// const baseUrl = 'https://socialmediastaging-1a776fnb.b4a.run'
+
 // apiUrl: 'https://backend-green-three-34.vercel.app/api/v1'
 // apiUrl: 'https://srng5l6h-3000.use2.devtunnels.ms/api/v1',
+
 export const environment = {
   production: false,
   apiUrl: `${baseUrl}/api/v1`,
--- frontend/src/environments/environment.staging.ts
+++ frontend/src/environments/environment.staging.ts
@@ -1,6 +1,8 @@
-const baseUrl = 'https://backend-macluiggy-macluiggys-projects.vercel.app';
+const baseUrl = 'https://socialmediastaging-1a776fnb.b4a.run';
+
 // apiUrl: 'https://social-media-staging.up.railway.app/api/v1',
 // apiUrl: 'https://backend-macluiggy-macluiggys-projects.vercel.app/api/v1',
+// const baseUrl = 'https://backend-macluiggy-macluiggys-projects.vercel.app';
 
 export const environment = {
   production: false,
