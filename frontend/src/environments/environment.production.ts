f858e19223f776d456d81a23c785808b58cf6bc6
macluiggy <luiggy.macias@imagineapps.co>
2024-06-20 11:19:22 -0500

    chore: Update base URL for production environment
    This commit updates the base URL in the frontend/src/environments/environment.production.ts file to point to the new backend server for the production environment. The previous URL has been replaced with the correct URL for the social media production environment. This change ensures that the frontend application connects to the correct backend API endpoints in the production environment.

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,4 +1,5 @@
-const baseUrl = 'https://backend-green-three-34.vercel.app';
+const baseUrl = 'https://socialmedia-ac4q9weo.b4a.run';
+// const baseUrl = 'https://backend-green-three-34.vercel.app';
 // apiUrl: 'social-media-sosote.up.railway.app/api/v1',
 // apiUrl: 'https://backend-green-three-34.vercel.app/api/v1',
 export const environment = {
